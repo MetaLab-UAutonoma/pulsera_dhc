d157a94d610ca4054e72b69d3d7b9fddf247685f
FabianCR <fabian.ecr1999@gmail.com>
2025-11-20 12:39:12 -0300

    modificacion prueba gps y alertas

--- src/config.cpp
+++ src/config.cpp
@@ -3,6 +3,6 @@
 // ——— Definiciones de los extern ——— un cambio
 HardwareSerial simSerial(1);
 HardwareSerial gpsSerial(2); 
-const char*    telefonoDestino = "+56912345678";
+const char*    telefonoDestino = "+56950506913";
 
 bool  debugMode = false, lastDebugMode = false;
